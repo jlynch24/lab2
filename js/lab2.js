e519f40e59fb651439e5d300af929ddbeda2d127
jlynch24 <jlynch24@my.brookdalecc.edu>
2025-09-17 18:45:28 -0400

    Initial commit

---
+++ js/lab2.js
@@ -0,0 +1,27 @@
+const submitButton = document.getElementById("submit"); //Gets that object
+submitButton.addEventListener("click", function(event) {
+    //Pure havascript
+    const firstTextBox = document.getElementById(input1);
+    const secondTextBox = document.getElementById(input2);
+
+    var first = firstTextBox.value;
+    var second = secondTextBox.value;
+
+    //jQuery
+    var third = $('#input3').val();
+
+    console.log("First: ", first);
+    console.log("Second: ", second);
+    console.log("Third: ", third);
+
+    alert(first +"\n" + second + "\n" + third);
+
+    //Stop form from firing
+    event.preventDefault(); //How is it is deprecated?
+});
+
+$('#clear').click(function() {
+    $("#input1").val("");
+    $("#input2").val("");
+    $("#input2").val("");
+});
